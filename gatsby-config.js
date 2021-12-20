fc3bfddb424200170e05602bf09c7c75d4e15e86
謝文瑜 <wenyu@xiewenyudeMacBook-Air.local>
2021-12-20 19:08:36 +0800

    fix style, test retrieve and render notion md

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,37 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My blog",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'My blog',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-remark",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
     {
-      resolve: "gatsby-source-notion-api",
+      resolve: 'gatsby-source-notion-api',
       options: {
         token: process.env.NOTION_API_TOKEN,
         databaseId: process.env.BLOG_ARTICLES_DB_ID,
@@ -39,4 +40,4 @@ module.exports = {
       },
     },
   ],
-};
+}
